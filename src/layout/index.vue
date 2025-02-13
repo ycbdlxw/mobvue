a9f20f00be30c43d5d682b8986bd3544e3e285f8
pany <panyang@mafengwo.com>
2025-02-13 20:15:41 +0800

    feat: 内容区滚动

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -10,9 +10,13 @@ const showTabbar = computed(() => route.meta.layout?.tabbar?.showTabbar)
 </script>
 
 <template>
-  <NavBar v-if="showNavBar" />
-  <router-view overflow-y-auto />
-  <Tabbar v-if="showTabbar" />
+  <div class="h-full flex flex-col">
+    <NavBar v-if="showNavBar" />
+    <div class="flex-1 overflow-y-auto">
+      <router-view />
+    </div>
+    <Tabbar v-if="showTabbar" />
+  </div>
 </template>
 
 <style scoped></style>
