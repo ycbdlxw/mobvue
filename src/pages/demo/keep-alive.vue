53766b3959d3fcf3346f8fbf31c48b6d7bcc3621
pany <939630029@qq.com>
2025-02-17 20:35:34 +0800

    feat: 为路由缓存示例添加 notice

--- src/pages/demo/keep-alive.vue
+++ src/pages/demo/keep-alive.vue
@@ -7,8 +7,14 @@ const value = ref<string>("")
 </script>
 
 <template>
-  <div un-p-20px>
-    <van-cell-group inset>
+  <div>
+    <van-notice-bar
+      color="var(--mobvue-primary-color)"
+      background="#ecf9ff"
+      left-icon="bulb-o"
+      text="基于 KeepAlive 组件实现的路由缓存，但不受 Layout 影响"
+    />
+    <van-cell-group inset un-mt-20px>
       <van-field v-model="value" placeholder="请输入" />
     </van-cell-group>
   </div>
