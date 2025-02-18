8a79442ee3dae674c5879a38cf90d91e3d93b534
pany <939630029@qq.com>
2025-02-18 14:47:26 +0800

    refactor: 抽离 Description 组件

---
+++ src/common/components/Description.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <h1 un-flex-y-center>
+      <img src="/favicon.png" un-w-38px un-h-38px>
+      <span un-ml-16px un-text-32px un-fw400>MobVue</span>
+    </h1>
+    <h2 un-color-hex-969799 un-text-14px un-fw400>
+      {{ props.text }}
+    </h2>
+  </div>
+</template>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
 import { demoRoutes } from "@/router"
+import Description from "@@/components/Description.vue"
 import Cell from "./components/Cell.vue"
 </script>
 
 <template>
   <div un-px-20px un-py-26px un-select-none>
-    <div un-pl-16px>
-      <h1 un-flex-y-center>
-        <img src="/favicon.png" un-w-38px un-h-38px>
-        <span un-ml-16px un-text-32px un-fw400>MobVue</span>
-      </h1>
-      <h2 un-color-hex-969799 un-text-14px un-fw400>
-        一个精心制作的移动端 H5 模板
-      </h2>
-    </div>
+    <Description text="一个精心制作的移动端 H5 模板" un-pl-16px />
     <div un-mt-40px>
       <span un-pl-16px un-color-hex-969799 un-text-14px un-fw400>
         示例集合
