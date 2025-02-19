4c6d6e9b1e42ff206037df0142733bcc73d1fa6a
pany <939630029@qq.com>
2025-02-19 13:36:06 +0800

    refactor: 减少路由守卫中的硬编码

--- src/router/guard.ts
+++ src/router/guard.ts
@@ -9,6 +9,8 @@ NProgress.configure({ showSpinner: false })
 
 const { setTitle } = useTitle()
 
+const LOGIN_PATH = "/login"
+
 export function registerNavigationGuard(router: Router) {
   // 全局前置守卫
   router.beforeEach((to, _from) => {
@@ -18,10 +20,10 @@ export function registerNavigationGuard(router: Router) {
       // 如果在免登录的白名单中，则直接进入
       if (isWhiteList(to)) return true
       // 其他没有访问权限的页面将被重定向到登录页面
-      return "/login"
+      return LOGIN_PATH
     }
     // 如果已经登录，并准备进入 Login 页面，则重定向到主页
-    if (to.path === "/login") return "/"
+    if (to.path === LOGIN_PATH) return "/"
     // 无限制访问
     return true
   })
@@ -29,7 +31,7 @@ export function registerNavigationGuard(router: Router) {
   router.afterEach((to) => {
     const keepAliveStore = useKeepAliveStore()
     // 清除所有路由缓存
-    if (to.path === "/login") keepAliveStore.delAllCachedRoutes()
+    if (to.path === LOGIN_PATH) keepAliveStore.delAllCachedRoutes()
     // 添加路由缓存
     keepAliveStore.addCachedRoute(to)
     // 设置标题
