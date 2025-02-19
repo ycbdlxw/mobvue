c5fee5271333a092085aad2335bbb94eec8653cc
pany <939630029@qq.com>
2025-02-19 16:21:08 +0800

    feat: 错误页面返回首页时采用 replace

--- src/pages/error/components/Layout.vue
+++ src/pages/error/components/Layout.vue
@@ -3,7 +3,7 @@
     <div un-w-250px un-mb-50px>
       <slot />
     </div>
-    <van-button type="primary" to="/">
+    <van-button type="primary" to="/" replace>
       回到首页
     </van-button>
   </div>
