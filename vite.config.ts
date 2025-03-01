29684310cab7a14d45bee1c28675cc0499cffc49
ClariS <1457715339@qq.com>
2025-03-01 20:32:07 +0800

    feat: support PWA (#6)
    Co-authored-by: pany <939630029@qq.com>

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import UnoCSS from "unocss/vite"
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite"
 import { defineConfig, loadEnv } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import svgLoader from "vite-svg-loader"
 
 // Configuring Vite: https://cn.vite.dev/config
@@ -121,6 +122,34 @@ export default defineConfig(({ mode }) => {
       Components({
         dts: "types/auto/components.d.ts",
         resolvers: [VantResolver()]
+      }),
+      // PWA
+      VitePWA({
+        includeAssets: ["favicon.png", "apple-touch-icon.png"],
+        manifest: {
+          name: "MobVue",
+          short_name: "MobVue",
+          description: "A crafted mobile template",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png"
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png"
+            },
+            {
+              src: "pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: ["any", "maskable"]
+            }
+          ]
+        }
       })
     ],
     // Configuring Vitest: https://cn.vitest.dev/config
